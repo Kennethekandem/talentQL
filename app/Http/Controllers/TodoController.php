c8d58e816e53d450701b483a0bf05eb0c3bcfd3b
Kenneth Ekandem <kennyekandem@gmail.com>
2021-01-06 10:15:40 +0100

    update

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -16,17 +16,20 @@ class TodoController extends Controller
 
     public function create(Request $request) {
 
+        // validate request
         $request->validate([
             'title' => 'required|max:100',
             'description' => 'required'
         ]);
 
+        // create a new column in todo table with the request data
         $create = Todo::create(array(
             'title' => $request['title'],
             'description' => $request['description'],
             'completed' => 0
         ));
 
+        // return response
         if($create) {
             return response()->json([
                 'status_code' => 200,
@@ -38,17 +41,22 @@ class TodoController extends Controller
 
     public function update(Request $request, $id) {
 
+        // find todo with id and update
         $update = Todo::findOrFail($id)->update($request->all());
 
         if($update) {
+
+            // find todo and get data
             $todo = Todo::find($id);
 
+            // return response
             return response()->json([
                 'status_code' => 200,
                 'message' => 'Todo updated successfully',
                 'data' => $todo
             ]);
         }else {
+            // return error if the request did not go through
             return response()->json([
                 'status_code' => 401,
                 'message' => 'Unable to update todo at the moment.'
@@ -60,6 +68,7 @@ class TodoController extends Controller
     {
         try
         {
+            // find and delete todo by id
             Todo::findOrFail($id)->delete();
             return response()->json([
                 'status_code' => 200,
@@ -69,6 +78,7 @@ class TodoController extends Controller
 
         catch (ModelNotFoundException $e)
         {
+            // throw error if id does not exist in todos table
             return response()->json([
                 'status_code' => 400,
                 'message' => 'Unable to delete todo at the moment'
